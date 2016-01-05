32914e85ccc66cafbb665555d7213c8d27de7055
Mohamed Lotfi SASSI <mohamedlotfi.sassi@gmail.com>
2016-01-05 17:51:34 +0100

    add to todos and todos/:id

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-//var PORT = 7832;
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+}, {
+    id: 3,
+    description: 'play foot',
+    completed: true
+}];
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
+});
+
+//Get /todos
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+//Get /todos/:id
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedToDo;
+    todos.forEach(function (todo) {
+        if (todoId === todo.id) {
+            matchedToDo = todo;
+        }
+    });
+       
+    if (matchedToDo) {
+        res.json(matchedToDo);
+    } else {
+        res.status(404).send();
+    }
+
 });
 
 app.listen(PORT, function () {
